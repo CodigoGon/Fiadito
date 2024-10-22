e34cc244b731fbef90e9aeb594b0cb104bacb550
kofvicio <tecnicopcgm@gmai.com>
2024-10-22 13:30:49 -0300

    abrir ventana diaria

---
+++ src/main/java/CodigoGon/Fiadito/Controller/DiariaController.java
@@ -0,0 +1,16 @@
+package CodigoGon.Fiadito.Controller;
+
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class DiariaController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
--- src/main/java/CodigoGon/Fiadito/Controller/IndexController.java
+++ src/main/java/CodigoGon/Fiadito/Controller/IndexController.java
@@ -1,6 +1,12 @@
 package CodigoGon.Fiadito.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -8,8 +14,24 @@ import java.util.ResourceBundle;
 
 @Component
 public class IndexController implements Initializable {
+
+    @FXML
+    private Button diaria;
+
+    @FXML
+    public void abrirDiaria() throws Exception {
+        Stage ventanaNueva = new Stage();
+        FXMLLoader cargarVentana = new FXMLLoader(getClass().getResource("/templates/Diaria.fxml"));
+        Parent root = cargarVentana.load();
+        Scene escena = new Scene(root);
+        ventanaNueva.setScene(escena);
+
+
+        ventanaNueva.show();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        
     }
 }
